a9de251d00acf3a4e52ce0a7a5f4c3323191bd73
Haruki1090 <haruharu.com.109607@gmail.com>
2024-02-09 11:12:06 +0900

    River-podの仕様に変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Todo App',
       home: TodoScreen(),
     );
   }
 }
 
-class TodoScreen extends StatefulWidget {
-  @override
-  _TodoScreenState createState() => _TodoScreenState();
-}
-
 class Todo {
-  String text;
+  String title;
   bool isDone;
+  DateTime dateTime;
 
-  Todo(this.text, this.isDone);
+    Todo(this.title, this.isDone, this.dateTime);
 }
 
-class _TodoScreenState extends State<TodoScreen> {
+class TodoScreen extends ConsumerWidget {
   TextEditingController _textController = TextEditingController();
-  List<Todo> _todoList = [];
+  final _todoListProvider = StateProvider<List<Todo>>((ref) => <Todo>[]);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<Todo> _todoList = ref.watch(_todoListProvider);
+
+    void _addTodo(ref, context) {
+      String newTodoTitle = _textController.text;
+      if (newTodoTitle.isNotEmpty) {
+        var newTodo = Todo(
+          newTodoTitle,
+          false,
+          DateTime.now(),
+        );
+        ref.read(_todoListProvider.notifier).state = [..._todoList, newTodo];
+        _textController.clear();
+      }
+    }
+
+    void _editTodo(int index, WidgetRef ref, BuildContext context) {
+      showDialog(context: context, builder: (context) {
+        return AlertDialog(
+          title: Text('編集'),
+          content: TextField(
+            controller: TextEditingController(text: _todoList[index].title),
+            onChanged: (value) {
+              var updatedTodoList = List<Todo>.from(_todoList);
+              updatedTodoList[index].title = value;
+              ref.read(_todoListProvider.notifier).state = updatedTodoList;
+            },
+          ),
+          actions: [
+            TextButton(
+              child: Text('キャンセル'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text('保存'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      });
+    }
+
+    void _deleteTodo(int index, WidgetRef ref, BuildContext context) {
+      showDialog(context: context, builder: (context) {
+        return AlertDialog(
+          title: Text('削除確認'),
+          content: Text('削除してもよろしいですか？'),
+          actions: [
+            TextButton(
+              child: Text('キャンセル'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text('削除'),
+              onPressed: () {
+                var updatedTodoList = List<Todo>.from(_todoList);
+                updatedTodoList.removeAt(index);
+                ref.read(_todoListProvider.notifier).state = updatedTodoList;
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      });
+    }
+
+    void _toggleDone(int index, bool value, WidgetRef ref, BuildContext context) {
+      List<Todo> updatedTodoList = List<Todo>.from(_todoList);
+      updatedTodoList[index].isDone = value;
+      ref.read(_todoListProvider.notifier).state = updatedTodoList;
+    }
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Todo App'),
@@ -61,9 +143,7 @@ class _TodoScreenState extends State<TodoScreen> {
                     ),
                     IconButton(
                       icon: Icon(Icons.add),
-                      onPressed: () {
-                        _addTodo();
-                      },
+                      onPressed: () => _addTodo(ref, context),
                     ),
                   ],
                 ),
@@ -78,26 +158,26 @@ class _TodoScreenState extends State<TodoScreen> {
                 return Card(
                   margin: EdgeInsets.all(8.0),
                   child: ListTile(
-                    title: Text(_todoList[index].text),
+                    title: Text(_todoList[index].title),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         Checkbox(
                           value: _todoList[index].isDone,
                           onChanged: (value) {
-                            _toggleDone(index, value!);
+                            _toggleDone(index, value!, ref, context);
                           },
                         ),
                         IconButton(
                           icon: Icon(Icons.edit),
                           onPressed: () {
-                            _editTodo(index);
+                            _editTodo(index, ref, context);
                           },
                         ),
                         IconButton(
                           icon: Icon(Icons.delete),
                           onPressed: () {
-                            _deleteTodo(index);
+                            _deleteTodo(index, ref, context);
                           },
                         ),
                       ],
@@ -110,77 +190,5 @@ class _TodoScreenState extends State<TodoScreen> {
         ],
       ),
     );
-  }
-
-  void _addTodo() {
-    setState(() {
-      String text = _textController.text;
-      if (text.isNotEmpty) {
-        _todoList.add(Todo(text, false));
-        _textController.clear();
-      }
-    });
-  }
-
-  void _editTodo(int index) {
-     showDialog(context: context, builder: (context) {
-        return AlertDialog(
-          title: Text('編集'),
-          content: TextField(
-            controller: TextEditingController(text: _todoList[index].text),
-            onChanged: (value) {
-              _todoList[index].text = value;
-            },
-          ),
-          actions: [
-            TextButton(
-              child: Text('キャンセル'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            TextButton(
-              child: Text('保存'),
-              onPressed: () {
-                setState(() {
-                  Navigator.pop(context);
-                });
-              },
-            ),
-          ],
-        );
-      });
-  }
-
-  void _deleteTodo(int index) {
-    showDialog(context: context, builder: (context) {
-      return AlertDialog(
-        title: Text('削除確認'),
-        content: Text('削除してもよろしいですか？'),
-        actions: [
-          TextButton(
-            child: Text('キャンセル'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          TextButton(
-            child: Text('削除'),
-            onPressed: () {
-              setState(() {
-                _todoList.removeAt(index);
-                Navigator.pop(context);
-              });
-            },
-          ),
-        ],
-      );
-    });
-  }
-
-  void _toggleDone(int index, bool value) {
-    setState(() {
-      _todoList[index].isDone = value;
-    });
   }
 }
